7d3aaeceeedcb629f1667abab1df18da23fc9c3b
TigerHee <316031602@qq.com>
2016-03-16 10:53:22 +0800

    biyesheji

---
+++ javascripts/tiger.js
@@ -0,0 +1,9 @@
+/**
+ * Created by tiger on 2016/3/15.
+ */
+$(function(){
+  var width = $(window).width();
+  var height = $(window).height();
+  $("body").css('height',height+'px');
+  $("body").css('width',width+'px');
+});
